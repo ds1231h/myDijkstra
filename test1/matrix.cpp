acbd88f8dc3e61d3baf9654db63dc6720ce423c6
Daniel Mu <ds1231h@163.com>
2017-04-13 20:38:27 +0800

    暂时性修改

--- test1/matrix.cpp
+++ test1/matrix.cpp
@@ -20,7 +20,7 @@
 
 // addition3
 // @subtitle: 重构2
-// @purpose: 代码重构--按照赋最大值作为判断是否在集合S内，并减少了for次数进一步优化
+// @purpose: 代码重构--按照赋最大值作为relax，并减少了for次数进一步优化
 // @author: MuPei
 // @version: 2.2
 // @data: 20170410
@@ -32,25 +32,9 @@
 // @version: 2.3
 // @data: 20170411
 
-// addition5
-// @subtitle: 重构4
-// @purpose: 以C++的STL最小优先队列实现优化（虽然觉得好像看不出短甚至更长了）
-// @author: MuPei
-// @version: 2.4
-// @data: 20170413
-
 #include <stdio.h>
 #include <iostream>
 #include <memory.h>
-#include<cstdio>
-#include<cstring>
-#include<cstdlib>
-#include<cmath>
-#include<algorithm>
-#include<stack>
-#include<queue>
-#include<vector>
-
 
 #if defined(_WIN32) || defined(WIN32) // win
 #define WINDOWS_IMPL
@@ -70,12 +54,6 @@ using namespace std;
 const int nodeNum = 6;
 const int maxLen = 1000;
 
-struct Node
-{
-	int num, val;   //存放结点编号和到初始点的距离 
-};
-
-
 /*
 function:		实现dijkstra算法，包括从最小队列抽取最小点和松弛操作
 parameter:
@@ -92,7 +70,7 @@ void Dijkstra(int beginNode, int *dist, int *prevN, int subDist[6][6])
 	int ss = 0;
 	int tmpLen = 0;
 	int sSet[6]; // 存放已经找到的节点
-	memset (sSet, -1, sizeof(sSet));
+	memset(sSet, -1, sizeof(sSet));
 	sSet[0] = beginNode;
 	
 	while (ss != nodeNum-1) // 循环nodeNum-1次即找到了nodeNum-1个节点的最短路径
@@ -113,7 +91,7 @@ void Dijkstra(int beginNode, int *dist, int *prevN, int subDist[6][6])
 			}
 		}
 		
-		// 将已在集合S内的点距源点距离改为最大值
+		// 松弛操作：通过赋最大值的方式将当前点去掉
 		for (int k = 0; k < nodeNum; ++k)
 		{
 			subDist[k][nextNode] = maxLen;
@@ -135,31 +113,34 @@ return:		void
 */
 void searchPath(int *prevN, int vNode, int uNode)
 {
-	int que[nodeNum]; // prevN是反向路径，这里倒一下是正向路径
-	int id = uNode;
-	int i = 0;
-	while (id != vNode)
+	// 用数组que保存一条路径信息
+	int que[nodeNum];
+	memset(que, 0, sizeof(que));
+	int tot = 0;
+	que[tot] = uNode;
+	tot++;
+	int tmp = prevN[uNode];
+	// 逐次从vNode连接到nNode
+	while (tmp != vNode)
 	{
-		que[i] = id + 1;
-		i++;
-		id = prevN[id];
+		que[tot] = tmp;
+		tmp = prevN[tmp];
+		tot++;
 	}
-	cout << id + 1 << " -> ";
-
-	for (i; i > 1; --i)
-	{
-		cout << " -> " << que[i - 1] << " -> ";
-	}
-	cout << que[i - 1] << endl;
-	
+	que[tot] = vNode;
+	for (int i = tot; i > -1; --i)
+		if (i != 0)
+			cout << que[i]+1 << " -> ";
+		else
+			cout << que[i]+1 << endl;
 }
 
 /*
-function:	邻接算法主函数
+function:	算法主函数
 parameter:	void
 return:		void
 */
-void matrixFun()
+void matrixFun(void)
 {
 	int dist[nodeNum];     // 表示当前点到源点的最短路径长度
 	int prevN[nodeNum];     // 记录当前点的前一个结点
@@ -219,94 +200,6 @@ void matrixFun()
 	cout << dist[5] << endl;
 }
 
-bool operator < (Node a, Node b)
-{
-	if (a.val == b.val)
-	{
-		return a.num > b.num;
-	}
-	return a.val>b.val;              //先出小 
-}
-
-void minQ()
-{
-	priority_queue<Node> qq;;   //优先从小到大
-	int dis[100];     //到原点最短距离 
-	int D[100][100];  //记录路径长度
-	
-	Node nod[6];
-	
-	while (!qq.empty())
-	{
-		qq.pop(); //清空
-	}
-	memset (D, -1, sizeof(D));
-
-	
-	D[1][2] = 8;
-	D[1][3] = 3;
-	D[1][4] = 2;
-	D[2][4] = 1;
-	D[2][5] = 3;
-	D[3][4] = 5;
-	D[3][6] = 6;
-	D[4][5] = 2;
-	D[5][6] = 1;
-
-	D[2][1] = 8;
-	D[3][1] = 3;
-	D[4][1] = 2;
-	D[4][2] = 1;
-	D[5][2] = 3;
-	D[4][3] = 5;
-	D[6][3] = 6;
-	D[5][4] = 2;
-	D[6][5] = 1;
-	
-	for (int i = 2; i <= nodeNum; i++)
-	{
-		dis[i] = maxLen;
-	}
-		
-	dis[1] = 0;
-	nod[0].num = 1;
-	nod[0].val = 0;
-
-	qq.push (nod[0]);   //将起点放入队列 
-
-	while (!qq.empty())  //不为空时 
-	{
-
-		for (int i = 2; i <= nodeNum; ++i)
-		{
-			int j = 1;
-			if (D[qq.top().num][i] != -1 && dis[i]>dis[qq.top().num] + D[qq.top().num][i])
-			{
-
-				dis[i] = dis[qq.top().num] + D[qq.top().num][i];
-				nod[j].num = i; nod[j].val = dis[i];
-				qq.push (nod[j]);
-			}
-			j++;
-		}
-
-		qq.pop();
-	}
-
-	for(int i = 2;i <= nodeNum;++i)
-	{
-	cout<<"初始点到"<<i<<"点的距离为："<<dis[i]<<endl;
-	}
-	
-/*
-	for (int i = 2; i <= V; i++)
-	{
-		cout << nod[i].num << "->";
-	}
-	cout << endl;*/
-}
-
-
 /*
 function:	测程序运行时间，支持win/Linux/Mac/openBSD等平台
 parameter:	void
@@ -323,19 +216,9 @@ void test_time_start(void)
 	double dfTim = 0;
 	QueryPerformanceFrequency(&litmp);
 	dfFreq = litmp.QuadPart;
-	
 	QueryPerformanceCounter(&litmp);
 	QPart1 = litmp.QuadPart;// 获得初始值
 	matrixFun();
-	QueryPerformanceCounter(&litmp);
-	QPart2 = litmp.QuadPart;//获得中止值
-	dfMinus = (double)(QPart2 - QPart1);
-	dfTim = dfMinus / dfFreq * 1000000;// 获得对应的时间值，单位为秒
-	cout << "用时" << dfTim << "us" << endl;
-
-	QueryPerformanceCounter(&litmp);
-	QPart1 = litmp.QuadPart;// 获得初始值
-	minQ();
 	QueryPerformanceCounter(&litmp);
 	QPart2 = litmp.QuadPart;//获得中止值
 	dfMinus = (double)(QPart2 - QPart1);
@@ -345,15 +228,8 @@ void test_time_start(void)
 	// gettimeofday()
 	struct timeval tpstart, tpend;
 	double timeuse;
-	
 	gettimeofday(&tpstart, NULL);
 	matrixFun();
-	gettimeofday(&tpend, NULL);
-	timeuse = 1000000 * (tpend.tv_sec - tpstart.tv_sec) + tpend.tv_usec - tpstart.tv_usec;//注意，秒的读数和微秒的读数都应计算在内
-	cout << "用时" << timeuse << "us" << endl;
-
-	gettimeofday(&tpstart, NULL);
-	minQ();
 	gettimeofday(&tpend, NULL);
 	timeuse = 1000000 * (tpend.tv_sec - tpstart.tv_sec) + tpend.tv_usec - tpstart.tv_usec;//注意，秒的读数和微秒的读数都应计算在内
 	cout << "用时" << timeuse << "us" << endl;
